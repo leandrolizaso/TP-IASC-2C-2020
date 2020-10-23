6014d6730fb4a6a56fb9cf2069b6d75b9b3ce265
PacoErik <erikpacocolque@est.frba.utn.edu.ar>
2020-10-22 22:32:06 -0300

    Cliente y servidor básicos, aún no están implementados los grupos y borrado/modificación de mensajes

---
+++ node-pigeon-basic-server/server.js
@@ -0,0 +1,147 @@
+const http = require("http").createServer();
+const io = require("socket.io")(http);
+const { nanoid } = require("nanoid");
+const port = 3000;
+const log = console.log;
+
+const chats = new Map();
+// {key: 6 character nanoid, value: {messages: [], users: [], isGroup: Boolean}}
+
+const users = new Map();
+// {key: username, value: socket id}
+
+function generateID() {
+    return nanoid(6);
+}
+
+function registerUser(username, id) {
+    users.set(username, id);
+}
+
+function isUserConnected(username) {
+    return users.has(username);
+}
+
+function authorizeUser(username, socket) {
+    const connected = isUserConnected(username)
+    if (isUserConnected(username)) {
+        log(username + " was already connected");
+    } else {
+        log(username + " connected");
+        registerUser(username, socket.id);
+    }
+    socket.emit("authorization", connected);
+}
+
+function isPrivateChat(chat, username, otherUsername) {
+    return !chat.isGroup && chat.users.includes(username) && chat.users.includes(otherUsername);
+}
+
+function getPrivateChat(username, otherUsername) {
+    const privateChat = [... chats].find(([_, chat]) => isPrivateChat(chat, username, otherUsername));
+    return privateChat ? privateChat.id : null;
+}
+
+function createPrivateChat(users) {
+    const id = generateID();
+    chats.set(id, {messages: [], users: users, isGroup: false});
+    return id;
+}
+
+function inviteUserToChat(username, otherUsername, chatID) {
+    const socketID = users.get(otherUsername);
+    io.to(socketID).emit("chat-invite", {
+        username: username, 
+        id: chatID
+    });
+}
+
+function isChatUser(chat, username) {
+    return chat.users.includes(username);
+}
+
+function getUserChats(username) {
+    const userChats = [... chats].filter(([_, chat]) => isChatUser(chat, username))
+    return userChats.map(([id, chat]) => {return {
+        id: id,
+        users: chat.users,
+        group: chat.isGroup
+    }});
+}
+
+function isEmpty(array) {
+    return array.length === 0;
+}
+
+function sendChatMessages(username, chatID) {
+    const socketID = users.get(username);
+    const chat = chats.get(chatID);
+    if (!isEmpty(chat.messages))
+        io.to(socketID).emit("messages", chat.messages);
+}
+
+function saveMessage(room, envelope) {
+    const chat = chats.get(room);
+    chat.messages.push(envelope);
+}
+
+io.on("connection", (socket) => {
+
+    const username = socket.handshake.query.username;
+
+    authorizeUser(username, socket);
+
+    socket.on("message", (data) => {
+        log(data);
+        const envelope = {
+            message: data.message,
+            username: username
+        }
+        socket.to(data.room).emit("message", envelope);
+        saveMessage(data.room, envelope);
+    })
+
+    socket.on("chat-with", (otherUsername) => {
+        log(username + " wants to chat with " + otherUsername);
+        if (isUserConnected(otherUsername)) {
+            let chatID = getPrivateChat(username, otherUsername);
+            if (!chatID)
+                chatID = createPrivateChat([username, otherUsername]);
+            socket.join(chatID);
+            socket.emit("chat-with", chatID);
+            sendChatMessages(username, chatID);
+            inviteUserToChat(username, otherUsername, chatID);
+        } else
+            socket.emit("chat-with", null);
+    })
+
+    socket.on("join-chat", (id) => { 
+        log(username + " wants to join chat " + id);
+        const availableChat = chats.has(id);
+        if (availableChat)  {
+            socket.join(id);
+            sendChatMessages(username, id);
+        }
+        socket.emit("join-chat", {
+            available : availableChat,
+            id: id
+        });
+    })
+
+    socket.on("query-chats", () => { 
+        log(username + " wants to know his chats ");
+        socket.emit("query-chats", getUserChats(username));
+    })
+
+    socket.on("leave-chat", (id) => { 
+        log(username + " wants to leave chat " + id);
+        socket.leave(id);
+    })
+
+    socket.on("disconnect", () => {
+        users.delete(username);
+        log(username + " disconnected");
+    })
+})
+
+http.listen(port, () => log("Server listening on port: " + port));
---
+++ node-pigeon-client/client.js
@@ -0,0 +1,221 @@
+const serverURL = "http://localhost:3000";
+const socketIO = require("socket.io-client");
+const chalk = require("chalk");  
+const lineReader = require("serverline")
+const connection = {socket: null, username: null, room: null};
+ 
+lineReader.init()
+lineReader.setCompletion(["/help", "/login", "/chatWith", "/joinChat", "/chats", "/leave", "/logoff"])
+lineReader.setPrompt(getPrompt());
+
+function getPrompt() {
+  return chalk.cyanBright("> ");
+}
+
+function getWords(str) {
+  return str.split(" ");
+}
+
+function connectToServer(username) {
+  return socketIO(serverURL, {
+    query: { username: username }
+  })
+}
+
+function send(event, data) {
+  connection.socket.emit(event, data);
+}
+
+function login(username) {
+  connection.socket = connectToServer(username);
+  connection.username = username;
+  addConnectionEvents();
+}
+
+function joinChat(id) {
+  send("join-chat", id);
+}
+
+function chatWith(username) {
+  send("chat-with", username);
+}
+
+function showChats() {
+  send("query-chats");
+}
+
+function leaveChat() {
+  send("leave-chat", connection.room);
+  connection.room = null;
+  console.log(chalk.yellow("Chat left successfully"));
+}
+
+function disconnect() {
+  connection.socket.disconnect();
+  connection.username = null;
+  console.log(chalk.magenta("Logged off!"));
+}
+
+function loggedIn() {
+  return connection.username !== null;
+}
+
+function connectedToRoom() {
+  return connection.room !== null;
+}
+
+function sendMessage(message) {
+  const cleanMessage = message.split("\n")[0];
+  console.log(chalk.gray(connection.username) + ": " + cleanMessage);
+  send("message", {
+    message: cleanMessage,
+    room: connection.room
+  })
+}
+
+function isEmpty(array) {
+  return array.length === 0;
+}
+
+lineReader.on("line", function(line) {
+
+  const words = getWords(line);
+  const cmd = words[0] || "";
+  const parameter1 = words[1] || "";
+
+  if (connectedToRoom() && line[0] !== "/") {
+    sendMessage(line);
+    return;
+  }
+
+  switch (cmd) {
+    case "/help":
+      console.log(chalk.yellow("/login [username]") + ": logs in with specified username");
+      console.log(chalk.yellow("/chatWith [username]") + ": start chatting with username or group");
+      console.log(chalk.yellow("/joinChat [id]") + ": joins a chat");
+      console.log(chalk.yellow("/chats") + ": shows your chats");
+      console.log(chalk.yellow("/leave") + ": leaves current chat");
+      console.log(chalk.yellow("/logoff") + ": logs off from current user");
+      break;
+    case "/login":
+      if (!loggedIn())
+        login(parameter1)
+      else
+        console.log(chalk.red("You must first /logoff to log in again."))
+      break;
+    case "/chatWith":
+      if (loggedIn() && !connectedToRoom())
+        chatWith(parameter1);
+      else
+        console.log(chalk.red("Couldn't execute that command. Not logged in or already chatting."))
+      break;
+    case "/joinChat":
+      if (loggedIn() && !connectedToRoom())
+        joinChat(parameter1);
+      else
+        console.log(chalk.red("Couldn't execute that command. Not logged in or already chatting."))
+      break;
+    case "/chats":
+      if (loggedIn())
+        showChats();
+      else
+        console.log(chalk.red("You must first /login to query your chats."))
+      break;
+    case "/leave":
+      if (loggedIn() && connectedToRoom())
+        leaveChat();
+      else
+        console.log(chalk.red("Couldn't execute that command. There's no active chat."));
+      break;
+    case "/logoff":
+      if (loggedIn())
+        disconnect();
+      else
+        console.log(chalk.red("You must be logged in."))
+      break;
+    default:
+      console.log(chalk.red("Unknown command. Please type /help for more information."));
+  }
+})
+ 
+lineReader.on("SIGINT", function(rl) {
+  rl.question("Do you really want to quit? (y/n): ", (answer) => {
+    if (answer.match(/^y(es)?$/i)) 
+      process.exit(0);
+    console.log();
+  });
+})
+
+function addSocketEvent(eventName, callback) {
+  connection.socket.on(eventName, callback);
+}
+
+function addConnectionEvents() {
+  addSocketEvent("authorization", () => {
+    console.log(chalk.green("Login success"));
+    username = connection.socket.io.opts.query.username;
+    addChatEvents();
+  })
+
+  addSocketEvent("reconnecting", (reason) => {
+    if (reason !== "io client disconnect")
+      console.log(chalk.red("Network error. Retrying connection..."));
+  })
+
+  addSocketEvent("reconnect", () => {
+    connection.socket.off();
+    addConnectionEvents();
+    addChatEvents();
+  })
+}
+
+function addChatEvents() {
+  addSocketEvent("message", (envelope) => {
+    const { message, username } = envelope;
+    console.log(chalk.blueBright(username) + ": " + message);
+  })
+
+  addSocketEvent("chat-with", (chatID) => {
+    if (chatID) {
+      connection.room = chatID;
+      console.log(chalk.yellow("Joined to chat #" + chatID));
+    } else
+      console.log(chalk.red("User is not connected, couldn't send invitation."));
+  })
+
+  addSocketEvent("join-chat", (result) => {
+    if (result.available) {
+      console.log(chalk.green("Joined to chat!"));
+      connection.room = result.id;
+    } else
+      console.log(chalk.red("Couldn't join that chat"));
+  })
+
+  addSocketEvent("query-chats", (chats) => {
+    if (isEmpty(chats))
+      console.log(chalk.red("No chats found"));
+    else
+      chats.forEach(chat => {
+        const [user, anotherUser] = chat.users;
+        console.log(chalk.yellow(chat.id + " - " + "Chat between " + user + " & " + anotherUser));
+      })
+  })
+
+  addSocketEvent("chat-invite", (invite) => {
+    if (invite.username !== connection.username)
+      console.log(chalk.yellow(invite.username + " invited you to join a private chat. Type /joinChat " + invite.id));
+    else
+    console.log(chalk.yellow("Nice, a chat only for (you)!"));
+  })
+
+  addSocketEvent("messages", (envelopes) => {
+    envelopes.forEach(envelope => {
+      if (envelope.username === connection.username)
+        console.log(chalk.gray(envelope.username) + ": " + envelope.message);
+      else
+        console.log(chalk.blueBright(envelope.username) + ": " + envelope.message);
+    })
+  })
+}
+
+  
