760eaf0455b102b9723fdbd065f0046f271efb89
pacoerik <erikpacocolque@est.frba.utn.edu.ar>
2020-11-24 18:35:08 -0300

    Actualización de script de deploy de back

--- scripts/run_master_balancers_pubsub.sh
+++ scripts/run_master_balancers_pubsub.sh
@@ -1,9 +1,9 @@
 echo "Running redis (if redis image isn't present it will be downloaded first)..."
 sudo docker run -d -p 6379:6379 --name iascgrupo1-redis redis 
 echo "Running main balancer..."
-sudo docker run -d -p 4001:4001 --name iascgrupo1-balancer iascgrupo1/balancer
+sudo docker run -d --network="host" iascgrupo1/balancer 4001
 echo "Running backup balancer..."
-sudo docker run -d -p 4002:4002 --name iascgrupo1-balancer_backup iascgrupo1/balancer_backup
+sudo docker run -d --network="host" iascgrupo1/balancer 4002 4001
 echo "Running master..."
 sudo docker run -d --network="host" -v $(which docker):/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock --name iascgrupo1-master iascgrupo1/master 5000
 echo "Running master backup..."
